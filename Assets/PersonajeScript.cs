0a1da082a07f4182e69b80c298cec95fcfa6baff
Pepe08tn <48521743@est.ort.edu.ar>
2024-06-06 10:45:52 -0300

    Alimentos creados, medidor de alimentos

---
+++ Assets/PersonajeScript.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonajeScript : MonoBehaviour
+{
+    public int nivelHambre;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TomarAliento(int valorAlimentario)
+    {
+        nivelHambre -= valorAlimentario;
+        if(nivelHambre <= 0)
+        {
+            nivelHambre = 0;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        AlimentoScript alimento;
+        alimento = other.GetComponent<AlimentoScript>();
+        if(alimento)
+        {
+            TomarAliento(alimento.valorAlimentario);
+            Destroy(other.gameObject);
+        }
+    }
+}
